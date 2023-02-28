5044b9f5ef41ff3d85b291f5ea19dfc11132b8c6
DESKTOP-1SPG7AQ\kosmo <tjddntjr98@naver.com>
2023-02-28 18:22:43 +0900

    no message

--- main/SelectFood.java
+++ main/SelectFood.java
@@ -6,6 +6,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class SelectFood {
@@ -18,25 +19,34 @@ public class SelectFood {
 	void load() { // 파일 내용을 hashmap으로 생성
 		Scanner sc = new Scanner(System.in);
 		File file1 = new File("src\\main\\Food.txt"); //음식 데이터
-		try(
-				Reader reader = new FileReader(file1);
-				BufferedReader br = new BufferedReader(reader);
-				){
+		try
+				{
+			Reader reader = new FileReader(file1);
+			BufferedReader br = new BufferedReader(reader);
 			String line = "";
 			while((line = br.readLine())!= null) {
 				String[] fn = line.split("/"); // 음식이름과 조건을 나눔
 				String name = fn[0];
-				String[] fc = fn[1].split(",");
+				String[] fc = fn[1].split(",");// 세가지의 조건을 나눔
 				sfFood.put(name, new data(fc[0] , fc[1], fc[2])); 
 				while(true) {
 					System.out.print("한식,중식,일식,양식중 하나를 입력해주세요.  -> ");
 					String category1 = sc.next();
-					if(fc[0].contains(category1)) {	
-						System.out.println("존재");
+					System.out.print("매운걸 원하시나요?(예/아니요)  ->  ");
+					String strong = sc.next();
+					System.out.print("종류를 선택해주세요.(밥,면,빵,기타) ->  ");
+					String type = sc.next();
+					if(fc[0].contains(category1)&&fc[1].contains(strong)&&fc[2].contains(type)) {	
+						Iterator<String> keys = sfFood.keySet().iterator();
+						while(keys.hasNext()) {
+							String key = keys.next();
+							System.out.println(key);
+						}
 					} else { 
 						System.out.println("잘못입력하신것 같습니다. 다시입력해주세요 ㅜㅡㅜ");
 						continue;
 					}
+		
 				}
 			}
 			reader.close();
